67ed60d199e8039d5905700fb704a0abc4316824
sreypovsum <52944272+sreypovsum@users.noreply.github.com>
2020-08-08 11:40:00 +0700

    update topbar

--- admin/includes/topheader.php
+++ admin/includes/topheader.php
@@ -1,42 +1,71 @@
-<div class="topbar">
-    <!-- LOGO -->
-    <div class="topbar-left">
-        <a href="index.html" class="logo"><span>NEWS<span>Top</span></span><i class="mdi mdi-layers"></i></a>
-        <!-- Image logo -->
-        <a href="index.html" class="logo">
-            <span>
-                <img src="assets/images/logo.png" alt="" height="30">
-            </span>
-            <!-- <i>
-                <img src="" alt="" height="28">
-            </i> -->
-        </a>
-    </div>
-    <!-- Button mobile view to collapse sidebar menu -->
-    <div class="navbar navbar-default" role="navigation">
-        <div class="container">
-            <ul class="nav navbar-nav navbar-left">
-                <li>
-                    <button class="button-menu-mobile open-left waves-effect">
+<div class="main-wrapper">
+   <div class="topbar" style="background-color: #ffffff !important; height:70px;">
+      <!-- LOGO -->
+            <div class="topbar-left">
+                <a href="index.html" class="logo"><span>NEWS<span>Top</span></span><i class="mdi mdi-layers"></i></a>
+                <!-- Image logo -->
+                <a href="index.html" class="logo">
+                    <span>
+                    <img src="assets/images/logo.png" alt="" height="30">
+                    </span>
+                </a>
+            </div>
+        
+            
+         <!-- top right -->
+         <!-- <nav class="top-navbar navbar-expand-md navbar-dark"> -->
+        <div class="topbar-right">
+            <div class="navbar-header" style="line-height: 65px; min-width: 70px;border-right: 1px solid rgba(0,0,0,.1);">
+               <div class="" role="navigation">
+               <ul class="nav navbar-nav navbar-left">
+                  <li style="height: 70px;">
+                     <button class="button-menu-mobile open-left waves-effect" style="margin-top: -15px; padding: 0 px 0px; line-height: 50px;">
                         <i class="mdi mdi-menu"></i>
-                    </button>
-                </li>
+                     </button>
+                  </li>
+               </ul>
+               </div>
+            </div>
+         <!-- Right(Notification) -->
+        <div class="navbar-collapse">
+            <ul class="nav navbar-nav mr-auto">
+               <li></li>
             </ul>
-            <!-- Right(Notification) -->
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown user-box">
-                    <a href="" class="dropdown-toggle waves-effect user-link" data-toggle="dropdown" aria-expanded="true">
-                        <img src="assets/images/users/avatar-1.jpg" alt="user-img" class="img-circle user-img">
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-right arrow-dropdown-menu arrow-menu-right user-list notify-list">
-                        <li>
-                            <h5>Hi, Admin</h5>
-                        </li>
-                        <li><a href="change-password.php"><i class="ti-settings m-r-5"></i> Chnage Password</a></li>
-                        <li><a href="logout.php"><i class="ti-power-off m-r-5"></i> Logout</a></li>
-                    </ul>
-                </li>
-            </ul> <!-- end navbar-right -->
-        </div><!-- end container -->
-    </div><!-- end navbar -->
+            <div class="nav nav-item hidden-sm-up" style="height:70px; float: right;">
+               <ul>
+                  <li class="dropdown user-box">
+                     <a href="" class="dropdown-toggle waves-effect user-link" data-toggle="dropdown" aria-expanded="true" style="margin-top:-10px;">
+                     <img src="assets/images/usericon.png" alt="user-img" class="img-circle user-img">
+                     </a>
+                     <ul class="dropdown-menu dropdown-menu-right arrow-dropdown-menu arrow-menu-right user-list notify-list" style="padding: 0px 0px 0px 0px;">
+                        <!-- <span class="with-arrow">
+                        <span class="bg-primary"></span> -->
+                        </span>
+                        <div class="d-flex no-block align-items-center p-15 bg-pf text-white m-b-10" style="height: 100px;width: 248px;">
+                           <li class="block-pf"> 
+                           <img src="assets/images/usericon.png" alt="user-img" class="img-circle user-img">
+                           </li>
+                           <div class="m-l-10 pf-name">
+                           <h4 class="m-b-0">Steave Jobs</h4>
+                           <p class=" m-b-0">varun@gmail.com</p>
+                           </div>
+                        </div>
+                        <li><a href=""><i class="ti-user icon-size"></i> My profile </a></li>
+                        <li><a href="change-password.php"><i class="ti-settings icon-size"></i> Chnage Password</a></li>
+                        <div class="dropdown-divider"></div>
+                        <li><a href="logout.php"><i class="ti-power-off icon-size"></i> Logout</a></li>
+                     </ul>
+                  </li>
+               </ul>
+               <ul class="nav" style="height:70px;">
+                  <li class="nav-item right-side-toggle" style="font-size:18px; ">
+                     <a class="nav-link  waves-effect waves-light" href="javascript:void(0)"><i class="ti-settings"></i></a>
+                  </li>
+               </ul>
+            </div>
+            
+         </div>
+         <!-- </nav> -->
+      </div>
+   </div>
 </div>
