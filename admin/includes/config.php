9d126499c1ee0977c6e11210974b9f22e58a0641
Chhinvanchhai <50760213+Chhinvanchhai@users.noreply.github.com>
2020-12-15 14:26:59 +0700

    add git ignore

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -2,6 +2,7 @@
 define('DB_SERVER','localhost');
 define('DB_USER','root');
 define('DB_PASS' ,'');
+
 define('DB_NAME','news');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
